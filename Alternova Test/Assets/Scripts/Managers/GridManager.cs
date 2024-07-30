b608e76bd26d05b7ad34f827eb35a5373ce8dcdf
julianosoriosalebe <julianosoriosalebe@gmail.com>
2024-07-30 17:45:22 -0500

    Commented Shuffle Mode for  Constant Testing of Current JSON

--- Alternova Test/Assets/Scripts/Managers/GridManager.cs
+++ Alternova Test/Assets/Scripts/Managers/GridManager.cs
@@ -49,7 +49,7 @@ public class GridManager : MonoBehaviour
 
         if (ValidateBlocks())
         {
-            ShuffleBlocks();
+            //ShuffleBlocks();
             GenerateGrid();
         }
         else
